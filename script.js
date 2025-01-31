16a2e43c438a391110a526c3fe81ca4314f8a9f2
juuvuor <96438568+juuvuor@users.noreply.github.com>
2025-01-31 12:52:10 +0200

    alkuun

---
+++ script.js
@@ -0,0 +1,7 @@
+function logUpdatedText() {
+    let hetu = document.getElementById("hetu").textContent;
+    console.log('%c hetu: ' + hetu, 'color: red;');
+}
+
+// Call this function whenever you need to log the updated text
+logUpdatedText();
