82155b797373ca59b1d17f11287b28953239910c
juuvuor <96438568+juuvuor@users.noreply.github.com>
2025-02-02 10:59:07 +0200

    muotoilu alkuun ja textarea regoimaan

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 function logUpdatedText() {
   let hetu = document.getElementById("hetu").value;
-  const addValue = document.getElementById("tulos");
-  addValue.innerHTML = hetu;
+  if (hetu.length < 10) {
+        console.log(hetu)
+        showWarning();
+    }
+}
+
+function showWarning(){
+    let warningElement = document.getElementById("warning");
+    warningElement.textContent = "Tarkista henkilötunnus!";
+    warningElement.classList.add("warning");
+    setTimeout(clearWarning,4000);
+}
+
+function clearWarning() {
+    let warningElement = document.getElementById("warning");
+    warningElement.textContent = "";
+    warningElement.classList.remove("warning");
+    clearTimeout();
+}
+
+function displayDate() {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1; 
+  let year = date.getFullYear();
+  let formattedDate = `${day}.${month}.${year}`;
+  document.getElementById("tulos").innerHTML = formattedDate;
 }
